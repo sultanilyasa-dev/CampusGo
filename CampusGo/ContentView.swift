5bf2430a1eb0fd3b080ccfc459af9ad9a766cc05
Sultan <sultanilyasa201@gmail.com>
2026-05-12 15:33:29 +0700

    fix:hotfix

--- CampusGo/ContentView.swift
+++ CampusGo/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("This is hotfix")
+            Text("This is hotfix")
         }
         .padding()
     }
