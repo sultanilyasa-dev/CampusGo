c40f6bb1e308b7b339537197df1f7842f1f656fe
Sultan <sultanilyasa201@gmail.com>
2026-05-12 14:31:49 +0700

    Initial Commit

---
+++ CampusGo/CampusGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusGoApp.swift
+//  CampusGo
+//
+//  Created by Training-06 on 12/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CampusGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CampusGo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CampusGo
+//
+//  Created by Training-06 on 12/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
